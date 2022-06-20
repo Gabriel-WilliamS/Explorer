08c3212898b1cd80b451a5d5d5337bf0bdeba43f
Gabriel William <gabrielwilliamcps@gmail.com>
2022-06-20 15:52:57 -0300

    Novo caminho de rota

--- Stage6-SPAUniverse/js/index.js
+++ Stage6-SPAUniverse/js/index.js
@@ -1,11 +1,11 @@
 import { Routes } from "./routes.js";
 
 const routes = new Routes();
-routes.add("/index.html", "/pages/home.html");
-routes.add("/", "/pages/home.html");
-routes.add("/universe", "/pages/universe.html");
-routes.add("/exploration", "/pages/exploration.html");
-routes.add(404, "/pages/404.html");
+routes.add("./Explorer/Stage6-SPAUniverse/index.html", "./Explorer/Stage6-SPAUniverse/pages/home.html");
+routes.add("./Explorer/Stage6-SPAUniverse/", "Explorer/Stage6-SPAUniverse/pages/home.html");
+routes.add("./Explorer/Stage6-SPAUniverse/universe", "Explorer/Stage6-SPAUniverse/pages/universe.html");
+routes.add("./Explorer/Stage6-SPAUniverse/exploration", "Explorer/Stage6-SPAUniverse/pages/exploration.html");
+routes.add("./Explorer/Stage6-SPAUniverse/404", "Explorer/Stage6-SPAUniverse/pages/404.html");
 
 window.route = () => routes.route();
 window.onpopstate = () => routes.handle();
